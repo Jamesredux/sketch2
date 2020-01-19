b866a0b12194a0917a44d30eb9ac35ce227a0225
James McCann <jamesredux@gmail.com>
2020-01-19 15:14:09 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sketch2
+Created as part of the odin project
+I have already created  this in jquery so I will now try to create a plain javascript version.
+
+
+https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project?ref=lnav
---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+// when dom is loaded this function is called to create squares
+// not sure why there is a false, copied it from stackoverflow
+
+document.addEventListener('DOMContentLoaded', () => {
+	drawGrid(32);
+}, false);
+
+
+
+function drawGrid(numSquare) {
+	clearContainer
+	
+	let squareSize = 600/numSquare + 'px';
+	console.log(squareSize);
+  const container = document.querySelector('.container');
+
+
+  	for (i = 1; i <= numSquare * numSquare; i++)  {
+  		let newSquare = document.createElement('div');
+  		newSquare.className = 'square';
+  		//newSquare.setAttribute('style', 'height: squareSize; width: 18px');
+  		newSquare.style.height = squareSize;
+  		newSquare.style.width = squareSize;
+  		newSquare.addEventListener('mouseenter', changeColor);
+  		//newSquare.addEventListener('mouseleave', revertColor)
+  		//add above as option for snake	
+  		container.appendChild(newSquare);
+			}
+	}
+
+
+function clearContainer() {
+	const divGroup = document.querySelectorAll('.square');
+
+	divGroup.forEach(div => {
+		div.classList.remove('.square');
+	} );
+
+}
+
+function defaultSketch(color) {
+	let drawColor = color
+	const square = document.querySelectorAll('.square')
+	square.addEventListener('mouseenter', () => {
+		square.style.backgroundColor = "#fff";
+	})
+	// add event listener to square class
+	// change color on mouse enter and remove on mouse leaeve
+	// body...
+}
+
+function changeColor() {
+  this.style.backgroundColor = "black";
+}
+
+function revertColor() {
+	this.style.backgroundColor = '#8fd8d2';
+}
